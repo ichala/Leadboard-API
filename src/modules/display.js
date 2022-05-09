1b050e6ec4f54a9625b4cffe9ff99ea323c66683
ichala <alijandhd@gmail.com>
2022-05-09 15:41:41 +0100

    Fix Linters

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,16 +1,14 @@
 const DisplayData = (players) => {
-  players = players.sort(function(a, b) {
-    return (b.score - a.score);
-});
+  players = players.sort((a, b) => (b.score - a.score));
   const PlayerList = document.querySelector('.PlayerList');
   PlayerList.innerHTML = '';
   if (players.length < 1) {
     PlayerList.innerHTML = 'No scores yet registered';
   } else {
-    players.forEach((player,i) => {
+    players.forEach((player, i) => {
       const NewPlayer = document.createElement('li');
       NewPlayer.classList.add('SinglePlayer');
-      NewPlayer.innerHTML = `<h3>${player.user} <sup class='text-red-900'>#${i+1}<sup/></h3>
+      NewPlayer.innerHTML = `<h3>${player.user} <sup class='text-red-900'>#${i + 1}<sup/></h3>
             <h4>${player.score}</h4>`;
       PlayerList.appendChild(NewPlayer);
     });
